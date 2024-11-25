0f7d030217283e1f56c47785859f92dc4ad44fb0
provash10 <provash20cb@gmail.com>
2024-11-26 01:39:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Class 33 Function
+// =================
+
+// function sum(){
+//     let x = 10;
+//     let y = 20;
+//     let result = x+y;
+//     console.log(result);
+// }
+// sum();
+
+function sum(number1, number2){
+    let x =number1;
+    let y = number2;
+    let result = x+y;
+    // console.log(result);
+    return result;
+}
+// sum(78, 22);
+// console.log(sum(78, 22));
+
+let sumation = sum(78, 22);
+console.log(sumation);
+
+let secondsum = sumation + 150;
+console.log(secondsum);
+
+let thirdsum = sum(38, 32);
+console.log(thirdsum);
+
+let subtraction = sub(100, 50);
+console.log(subtraction);
